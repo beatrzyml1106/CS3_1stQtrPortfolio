7467f9810084a1ca33378ea12ffac9faf79a5c28
beatrzyml1106 <b2029byatamon@pshs.edu.ph>
2025-08-05 08:57:14 +0800

    second commit

---
+++ public/script.js
@@ -0,0 +1,8 @@
+let age = 13;
+let maxAge = 100;
+let numPerDay = 2;
+let totalRequired;
+
+totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write("You will need " + totalRequired + " cups of coffee to last you until the age of " + maxAge)
