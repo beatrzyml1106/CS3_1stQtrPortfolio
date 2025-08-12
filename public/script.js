5bebc1b0d578df0b8d2c557d429bd52d5bbd032a
beatrzyml1106 <b2029byatamon@pshs.edu.ph>
2025-08-12 08:22:05 +0800

    fourth commit

--- public/script.js
+++ public/script.js
@@ -1,8 +1,14 @@
-let age = 13;
-let maxAge = 100;
-let numPerDay = 2;
 let totalRequired;
 
-totalRequired = (numPerDay * 365) * (maxAge - age);
+let age = window.prompt("Age?", "13");
+let maxAge =window.prompt("Max age?", "100")
+let numPerDay = window.prompt("How many cups of coffe per day?");
+
+if(confirm(`Is your input correct?`)) {
+    totalRequired = (numPerDay * 365) * (maxAge - age);
 
 document.write("You will need " + totalRequired + " cups of coffee to last you until the age of " + maxAge)
+}
+else{
+console.log("Run the code again")
+}
